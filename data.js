0c66a7204a0771b3e5606e132a1b06bf78b3b6c7
Hassaan-Mahmood <hassaan2980@gmail.com>
2026-08-18 23:55:45 +0500

    js

--- data.js
+++ data.js
@@ -80,7 +80,7 @@ var skillGroups = [
     category: "Front End",
     icon: "</>",
     skills: [
-      { name: "HTML5", percent: 92 },
+      { name: "HTML5", percent: 90 },
       { name: "CSS3", percent: 88 },
       { name: "JavaScript", percent: 80 },
       { name: "Bootstrap", percent: 75 },
